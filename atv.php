affcd7b38994626148a98475cbc7fc85a23ba21d
Rajarshi Guha <rajarshi.guha@gmail.com>
2010-08-21 23:29:38 +0000

    Added some code to return author frequencies based on tweets

--- atv.php
+++ atv.php
@@ -7,12 +7,24 @@ $search = new TwitterSearch($term);
 $search->user_agent = 'phptwittersearch:rajarshi.guha@gmail.com';
 $results = $search->rpp(50000)->results();
 
-$ret = "";
-foreach ($results as $result) {
-  if ($what == "text") {
+#print_r($results[0]);
+
+if ($what == "text") {
+  $ret = "";
+  foreach ($results as $result) {
     $ret = $ret . " " . $result->{'text'};
   }
+  echo $ret;
+} else if ($what == "author") {
+  $ret = array();
+  foreach ($results as $result) {
+    $author = $result->{'from_user'};
+    if ($ret[$author] == NULL) $ret[$author] = 1;
+    else $ret[$author] = $ret[$author]+1;
+  }
+  header('Content-type: application/json');
+  echo json_encode($ret);
 }
-echo $ret;
+
 ?>
 
